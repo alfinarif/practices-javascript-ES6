6d106e5f1164248c6b038044b8d47451febcb38e
alfinarif <cleavercode@gmail.com>
2024-12-22 01:08:33 +0300

    practice on going

--- main.js
+++ main.js
@@ -16,14 +16,12 @@
 
 // REST PARAMETER
 // function Calculation(a, b, ...numbers){
-//     let a1 = a; // a = 10; and b = 20;
-//     let b1 = b;
 //     let sum = 0;
 //     for(let i=0; i < numbers.length; i++){
 //         sum += numbers[i];
 //     }
 
-//     console.log(a1, b1, sum);
+//     console.log(a, b, sum);
 
 // }
 
@@ -47,12 +45,83 @@
 // }
 
 
+// FOR IN LOOP
+// const myObject = {name: "Alfin Arif", age: 25, country: "Bangladesh"};
 
-/// Hello world
+// for(let person in myObject){
+//     console.log(myObject[person]);
+// }
 
-// updated status
 
 
-// last updated from vs code
+// DECISION MAKING if else statement
+// const myObject = {name: "Alfin Arif", age: 25, country: "Bangladesh"};
 
-// last updated to check the git status
+// if (myObject.country == "Bangladesh"){
+//     console.log("I am from Bangladesh");
+// } 
+// else if(myObject.country == "Pakistan"){
+//     console.log("I am from Pakistan")
+// }
+// else if(myObject.country == "USA"){
+//     console.log("I am from USA");
+// }
+// else{
+//     console.log("Where are you from?");
+// }
+
+
+
+// ES6 FUNCTION DECLARATION
+// Simple Function
+function simpleFunc () {
+    let x = 10;
+    let y = 10;
+    let sum = x + y;
+    console.log(sum);
+}
+
+// simpleFunc();
+
+
+// Parameterized Function
+function paraFunc (a, b){
+    let sum = a + b;
+    console.log(sum);
+}
+
+// paraFunc(10, 20);
+
+
+// Rest Parameters Function
+function restParaFunc (...props){
+    console.log(props[5]);
+}
+
+// restParaFunc(1, 3, 5, 2, 7, "Alfin", "Arif");
+
+
+// Function Return Type
+function returnFunc (){
+    return "I am from Bangladesh";
+};
+const myFunc = returnFunc(); // we can store the function in a variable;
+// console.log(myFunc);
+
+
+
+// Anonymous Function
+const anonymousFunc = function(){
+    return "This is an anonymous function";
+}
+// console.log(anonymousFunc());
+
+
+// Arrow Function
+const arrowFunc = () => {
+    return "This is an arrow function";
+}
+console.log(arrowFunc());
+
+
+
