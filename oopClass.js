575365f563422b59ed35aff6db0ad452f9a9d269
alfinarif <cleavercode@gmail.com>
2024-12-22 01:36:30 +0300

    start oop

---
+++ oopClass.js
@@ -0,0 +1,73 @@
+
+class myClass {
+
+
+    myFunction() {
+        console.log("I am from function inside Class");
+    }
+
+    sumFunc(a, b){
+        console.log(a + b);
+    }
+
+
+}
+
+
+const myObject = new myClass();
+myObject.myFunction();
+myObject.sumFunc(3, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
