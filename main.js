a72c6d5113517fbb749e10f3d8afb0fa6b8c44d1
alfinarif <cleavercode@gmail.com>
2024-12-21 15:53:39 +0300

    practice day1

---
+++ main.js
@@ -0,0 +1,47 @@
+// RE-START JAVASCRIPT ONCE AGAIN
+// console.log("Hello World");
+// "use strict"
+// myName();
+
+// function myName(){
+//     let name = "Alfin Arif"
+// }
+
+
+// let poorCountry = ["Bangladesh", "Srilanka"];
+// let richCountry = [...poorCountry, "America", "Canada"];
+
+// console.log(richCountry);
+
+
+// REST PARAMETER
+// function Calculation(a, b, ...numbers){
+//     let a1 = a; // a = 10; and b = 20;
+//     let b1 = b;
+//     let sum = 0;
+//     for(let i=0; i < numbers.length; i++){
+//         sum += numbers[i];
+//     }
+
+//     console.log(a1, b1, sum);
+
+// }
+
+// Calculation(10, 20, 1, 2, 3, 4, 5);
+
+
+
+
+// DYNAMIC FUNCTION
+// const names = function(name){
+//     return name;
+// }
+
+// console.log(names("Alfin Arif"));
+
+
+
+// SIMPLE FOR LOOP
+// for(let i=0; i<5; i = i+1){
+//     console.log(i);
+// }
